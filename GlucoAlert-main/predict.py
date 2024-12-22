1eca38fa2c210e5312a087fa9f116238edf33a57
hamzah-1010 <hamzaleb22@gmail.com>
2024-12-22 23:11:08 +0200

    Update predict.py

--- GlucoAlert-main/predict.py
+++ GlucoAlert-main/predict.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 
 # creating a function for prediction
